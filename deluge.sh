bb48dae4157076dc181a56c38b21f7b9033fbb24
binhex <megalith01@gmail.com>
2015-02-04 14:30:16 +0000

    fix local ip grep command for arch

--- deluge.sh
+++ deluge.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# set tun local ip to empty string
-LOCAL_IP=""
+# set tun local ip to invalid ip
+LOCAL_IP="256.256.256.256"
 
 # loop and wait until tun local ip is valid
 until ipcalc -c $LOCAL_IP
 do
   sleep 0.1
-  LOCAL_IP=`ifconfig $DEVICE | grep 'inet addr:' | cut -d: -f2| cut -d' ' -f1`
+  LOCAL_IP=`ifconfig tun0 | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+'`
 done
- 
+
+echo "[info] tunnel local ip is $LOCAL_IP"
+
 # run deluge daemon
 /usr/bin/deluged -d -c /config -L info -l /config/deluged.log
 
@@ -21,6 +23,8 @@ done
 
 # set incoming port to specific value
 # /usr/bin/deluge-console -c /config "config --set listen_ports ($PORT,$PORT)"
+
+# echo "[info] incoming listening port is $PORT"
 
 # run deluge webui
 /usr/bin/deluge-web -c /config
