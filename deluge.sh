75c0112e817b93ea1ee3332a59fb763852076ec6
binhex <megalith01@gmail.com>
2015-02-04 13:05:14 +0000

    add package ipcalc to check valid ip for tun

--- deluge.sh
+++ deluge.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# sleep to allow openvpn to create tunnel (supervisor does not support dependencies)
-sleep 20
-
 # get tun ip address
 LOCAL_IP=`ifconfig $DEVICE | grep 'inet addr:' | cut -d: -f2| cut -d' ' -f1`
+
+# check tun lcoal ip is valid, if not then sleep
+ipcalc -c $LOCAL_IP
+echo $?
+ 
+# run deluge daemon first
+/usr/bin/deluged -d -c /config -L info -l /config/deluged.log
 
 # set listen interface for deluge to local ip for tunnel
 deluge-console -c /config "config --set listen_interface $LOCAL_IP"
@@ -14,12 +18,6 @@ deluge-console -c /config "config --set random_port False"
 
 # set incoming port to specific value
 # deluge-console -c /config "config --set listen_ports ($PORT,$PORT)"
-
-# check tunnel status is "UP"
-# ip link show tun0
- 
-# run deluge daemon first
-/usr/bin/deluged -d -c /config -L info -l /config/deluged.log
 
 # run deluge webui
 /usr/bin/deluge-web -c /config
