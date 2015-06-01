2dda7ce8a59e32c6e4ec119f37bec8f3ed14342a
binhex <megalith01@gmail.com>
2015-06-01 14:27:50 +0100

    allow deluge daemon access, set random port and listen on all when vpn disabled

--- apps/nobody/setip.sh
+++ apps/nobody/setip.sh
@@ -5,25 +5,35 @@ while [[ $(netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".58846"') == "" ]]; do
 	sleep 0.1
 done
 
-# while loop to check incoming port every 5 mins
-while true
-do
-	# run script to check ip is valid for tun0
-	source /home/nobody/checkip.sh
-	
-	# query deluge for current ip for tunnel
-	LISTEN_INTERFACE=`/usr/bin/deluge-console -c /config "config listen_interface" | grep -P -o -m 1 '[\d\.]+'`
-	
-	# if current listen interface ip is different to tunnel local ip then re-configure deluge
-	if [[ $LISTEN_INTERFACE != "$LOCAL_IP" ]]; then
+# if vpn set to "no" then set deluge to listen on all ip addresses
+if [[ $VPN_ENABLED == "no" ]]; then
 
-		echo "[info] Deluge listening interface IP $LISTEN_INTERFACE and OpenVPN local IP $LOCAL_IP different, configuring Deluge..."
-						
-		# set listen interface to tunnel local ip
-		/usr/bin/deluge-console -c /config "config --set listen_interface $LOCAL_IP"
-		
-	fi
-	
-	sleep 5m
-	
-done
+	echo "[info] VPN not enabled, configuring Deluge listen address to all IP addresses on the local machine"
+	/usr/bin/deluge-console -c /config "config --set listen_interface 0.0.0.0"
+
+else
+
+	# while loop to check incoming port every 5 mins
+	while true
+	do
+		# run script to check ip is valid for tun0
+		source /home/nobody/checkip.sh
+
+		# query deluge for current ip for tunnel
+		LISTEN_INTERFACE=`/usr/bin/deluge-console -c /config "config listen_interface" | grep -P -o -m 1 '[\d\.]+'`
+
+		# if current listen interface ip is different to tunnel local ip then re-configure deluge
+		if [[ $LISTEN_INTERFACE != "$LOCAL_IP" ]]; then
+
+			echo "[info] Deluge listening interface IP $LISTEN_INTERFACE and OpenVPN local IP $LOCAL_IP different, configuring Deluge..."
+
+			# set listen interface to tunnel local ip
+			/usr/bin/deluge-console -c /config "config --set listen_interface $LOCAL_IP"
+
+		fi
+
+		sleep 5m
+
+	done
+
+fi
--- start.sh
+++ start.sh
@@ -147,6 +147,11 @@ else
 		ip route add default via $DEFAULT_GATEWAY table privoxy
 	fi
 
+	# setup route for deluge daemon using set-mark to route traffic for port 58846 to eth0
+	#echo "58846    daemon" >> /etc/iproute2/rt_tables
+	#ip rule add fwmark 3 table daemon
+	#ip route add default via $DEFAULT_GATEWAY table daemon
+
 	echo "[info] ip route"
 	ip route
 	echo "--------------------"
@@ -159,7 +164,10 @@ else
 
 	# accept input to tunnel adapter
 	iptables -A INPUT -i tun0 -j ACCEPT
-
+	
+	# accept input to/from docker containers (172.x range is internal dhcp)
+	iptables -A INPUT -s 172.17.0.0/16 -d 172.17.0.0/16 -j ACCEPT
+	
 	# accept input to vpn gateway
 	iptables -A INPUT -i eth0 -p $VPN_PROTOCOL --sport $VPN_PORT -j ACCEPT
 
@@ -186,31 +194,34 @@ else
 	# set policy to drop for output
 	iptables -P OUTPUT DROP
 
-	# accept output to tunnel adapter
+	# accept output from tunnel adapter
 	iptables -A OUTPUT -o tun0 -j ACCEPT
 
-	# accept output to vpn gateway
+	# accept output to/from docker containers (172.x range is internal dhcp)
+	iptables -A OUTPUT -s 172.17.0.0/16 -d 172.17.0.0/16 -j ACCEPT
+	
+	# accept output from vpn gateway
 	iptables -A OUTPUT -o eth0 -p $VPN_PROTOCOL --dport $VPN_PORT -j ACCEPT
 
-	# accept output to deluge webui port 8112 (used when tunnel down)
+	# accept output from deluge webui port 8112 (used when tunnel down)
 	iptables -A OUTPUT -o eth0 -p tcp --dport 8112 -j ACCEPT
 	iptables -A OUTPUT -o eth0 -p tcp --sport 8112 -j ACCEPT
 
-	# accept output to deluge webui port 8112 (used when tunnel up)
+	# accept output from deluge webui port 8112 (used when tunnel up)
 	iptables -t mangle -A OUTPUT -p tcp --dport 8112 -j MARK --set-mark 1
 	iptables -t mangle -A OUTPUT -p tcp --sport 8112 -j MARK --set-mark 1
 
-	# accept output to privoxy port 8118 (used when tunnel up)
+	# accept output from privoxy port 8118 (used when tunnel up)
 	iptables -t mangle -A OUTPUT -p tcp --dport 8118 -j MARK --set-mark 2
 	iptables -t mangle -A OUTPUT -p tcp --sport 8118 -j MARK --set-mark 2
 
-	# accept output dns lookup
+	# accept output for dns lookup
 	iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
-	# accept output icmp (ping)
+	# accept output for icmp (ping)
 	iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 
-	# accept output to local loopback
+	# accept output from local loopback
 	iptables -A OUTPUT -o lo -j ACCEPT
 
 	echo "[info] iptables"
