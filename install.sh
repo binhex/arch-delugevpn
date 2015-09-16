e8ece144930ce1e4a5758d30c4aebf839d82b723
binhex <megalith01@gmail.com>
2015-09-16 17:02:53 +0100

    switch back to 1.3.11

--- install.sh
+++ install.sh
@@ -4,11 +4,16 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar librsvg pygtk python2-service-identity python2-mako python2-notify deluge"
+pacman_packages="unzip unrar librsvg pygtk python2-service-identity python2-mako python2-notify"
 
 # install pre-reqs
 pacman -Sy --noconfirm
 pacman -S --needed $pacman_packages --noconfirm
+
+# download and install package
+curl -L https://github.com/binhex/arch-packages/raw/master/deluge-1.3.11-3-any.pkg.tar.xz -o /tmp/deluge-1.3.11-3-any.pkg.tar.xz
+curl -L https://github.com/binhex/arch-packages/raw/master/deluge-1.3.11-3-any.pkg.tar.xz.sig -o /tmp/deluge-1.3.11-3-any.pkg.tar.xz.sig
+pacman -U /tmp/deluge-1.3.11-3-any.pkg.tar.xz --noconfirm
 
 # set permissions
 chown -R nobody:users /home/nobody /usr/bin/deluged /usr/bin/deluge-web
