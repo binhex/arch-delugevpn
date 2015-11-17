395b244fe41893495bdd2606672575037877dbef
binhex <megalith01@gmail.com>
2015-11-17 21:35:00 +0000

    remove sched plugin, not picked up

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ pacman -S --needed $pacman_packages --noconfirm
 chown -R nobody:users /home/nobody /usr/bin/deluged /usr/bin/deluge-web
 chmod -R 775 /home/nobody /usr/bin/deluged /usr/bin/deluge-web
 
-# install patched deluge scheduler plugin (bug with 1.3.12 release)
-curl -o /usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg https://github.com/binhex/arch-patches/releases/download/scheduler-0.2-py2.7/Scheduler-0.2-py2.7.egg
+# remove faulty scheduler plugin (bug with 1.3.12 release)
+rm -f /usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg
 
 # cleanup
 yes|pacman -Scc
