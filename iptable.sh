957b70d56af159dd6c88377e40818ed74f09c2de
binhex <megalith01@gmail.com>
2016-02-02 22:22:41 +0000

    strip out mangle, use lan network route

--- iptable.sh
+++ iptable.sh
@@ -3,52 +3,18 @@
 # ip route
 ###
 
-# check kernel for iptable_mangle module
-lsmod | grep "iptable_mangle" > /dev/null
-iptable_mangle_exit_code=$?
+if [[ ! -z "${LAN_NETWORK}" ]]; then
 
-# if iptable_mangle is available (kernel module) then set mark
-if [[ $iptable_mangle_exit_code == 0 ]]; then
-
-	echo "[info] iptable_mangle support detected, adding fwmark for tables"
-
-	# setup route for deluge webui using set-mark to route traffic for port 8112 to eth0
-	echo "8112    webui" >> /etc/iproute2/rt_tables
-	ip rule add fwmark 1 table webui
-	ip route add default via $DEFAULT_GATEWAY table webui
-
-	# setup route for privoxy using set-mark to route traffic for port 8118 to eth0
-	if [[ $ENABLE_PRIVOXY == "yes" ]]; then
-		echo "8118    privoxy" >> /etc/iproute2/rt_tables
-		ip rule add fwmark 2 table privoxy
-		ip route add default via $DEFAULT_GATEWAY table privoxy
-	fi
-
-	# setup route for deluge daemon using set-mark to route traffic for port 58846 to eth0
-	if [[ ! -z "${LAN_RANGE}" ]]; then
-		echo "58846    deluged" >> /etc/iproute2/rt_tables
-		ip rule add fwmark 3 table deluged
-		ip route add default via $DEFAULT_GATEWAY table deluged
-	fi
+	echo "[info] Adding ${LAN_NETWORK} as route via docker eth0"
+	ip route add "${LAN_NETWORK}" via "${DEFAULT_GATEWAY}" dev eth0
 
 else
-	
-	if [[ ! -z "${LAN_NETWORK}" ]]; then
 
-		echo "[info] iptable_mangle not supported, adding ${LAN_NETWORK} as route"
-
-		# add in route for lan network (env var) via docker eth0 for kernels without iptable_mangle
-		ip route add "${LAN_NETWORK}" via "${DEFAULT_GATEWAY}" dev eth0
-
-	else
-
-		echo "[crit] LAN network not defined, please specify via env variable LAN_NETWORK" && exit 1
-
-	fi
+	echo "[crit] LAN network not defined, please specify via env variable LAN_NETWORK" && exit 1
 
 fi
 
-echo "[info] ip routes defined..."
+echo "[info] ip routes defined as follows..."
 ip route
 echo "--------------------"
 
@@ -77,11 +43,9 @@ if [[ $ENABLE_PRIVOXY == "yes" ]]; then
 	iptables -A INPUT -i eth0 -p tcp --sport 8118 -j ACCEPT
 fi
 
-# accept input to deluge daemon port 58846 for lan range if specified
-if [[ ! -z "${LAN_RANGE}" ]]; then
-	iptables -A INPUT -i eth0 -m iprange --src-range $LAN_RANGE -j ACCEPT
-	iptables -A INPUT -i eth0 -m iprange --dst-range $LAN_RANGE -j ACCEPT
-fi
+# accept input to deluge daemon port 58846 for lan network
+iptables -A INPUT -i eth0 -s "${LAN_NETWORK}" -j ACCEPT
+iptables -A INPUT -i eth0 -d "${LAN_NETWORK}" -j ACCEPT
 
 # accept input dns lookup
 iptables -A INPUT -p udp --sport 53 -j ACCEPT
@@ -117,28 +81,9 @@ if [[ $ENABLE_PRIVOXY == "yes" ]]; then
 	iptables -A OUTPUT -o eth0 -p tcp --sport 8118 -j ACCEPT
 fi
 
-# if iptable mangle is available (kernel module) then use mark
-if [[ $iptable_mangle_exit_code == 0 ]]; then
-
-	echo "[info] iptable_mangle support detected, using mark"
-	
-	# accept output from deluge webui port 8112 (use mark to force connection over eth0 when tun up)
-	iptables -t mangle -A OUTPUT -p tcp --dport 8112 -j MARK --set-mark 1
-	iptables -t mangle -A OUTPUT -p tcp --sport 8112 -j MARK --set-mark 1
-
-	# accept output from privoxy port 8118 if enabled (use mark to force connection over eth0 when tun up)
-	if [[ $ENABLE_PRIVOXY == "yes" ]]; then
-		iptables -t mangle -A OUTPUT -p tcp --dport 8118 -j MARK --set-mark 2
-		iptables -t mangle -A OUTPUT -p tcp --sport 8118 -j MARK --set-mark 2
-	fi
-
-	# accept output from deluge daemon port 58846 for lan range if specified (use mark to force connection over eth0 when tun up)
-	if [[ ! -z "${LAN_RANGE}" ]]; then
-		iptables -t mangle -A OUTPUT -m iprange --dst-range $LAN_RANGE -j MARK --set-mark 3
-		iptables -t mangle -A OUTPUT -m iprange --src-range $LAN_RANGE -j MARK --set-mark 3
-	fi
-
-fi
+# accept input to deluge daemon port 58846 for lan network
+iptables -A OUTPUT -i eth0 -s "${LAN_NETWORK}" -j ACCEPT
+iptables -A OUTPUT -i eth0 -d "${LAN_NETWORK}" -j ACCEPT
 
 # accept output for dns lookup
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
@@ -149,6 +94,6 @@ iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 # accept output from local loopback adapter
 iptables -A OUTPUT -o lo -j ACCEPT
 
-echo "[info] iptables defined..."
+echo "[info] iptables defined as follows..."
 iptables -S
 echo "--------------------"
