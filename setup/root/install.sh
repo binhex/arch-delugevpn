7fe02a246d3431522abb6b75d87c7ff3fd08ff98
binhex <megalith01@gmail.com>
2016-03-15 15:00:49 +0000

    prevent removal of core packages when removing base-devel

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,13 +4,16 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify deluge"
+pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
 
 # remove faulty scheduler plugin (bug with 1.3.12 release)
 rm -f /usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg
+
+# call aor script (arch official repo)
+source /root/aor.sh
 
 # cleanup
 yes|pacman -Scc
