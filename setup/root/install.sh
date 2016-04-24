6aec47c31a884dbc0b229f0084eca155d7825880
paulpoco <paulpoco@gmail.com>
2016-04-24 12:08:14 -0700

    Update install.sh
    Install flexget from here

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,13 +4,18 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat"
+pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat python2-pip nano"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
 
 # remove faulty scheduler plugin (bug with 1.3.12 release)
 rm -f /usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg
+
+#install flextget
+pip2 install --upgrade setuptools
+pip2 install --upgrade flexget
+mkdir /home/nobody/.flexget
 
 # call aor script (arch official repo)
 source /root/aor.sh
