488dd8c268406f9429a2762a7a89c9ad5019878a
paulpoco <paulpoco@gmail.com>
2016-04-24 02:38:09 -0700

    Update flexget.sh

--- flexget.sh
+++ flexget.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "$(date "+%d.%m.%Y %T") : Starting DAEMON"
-flexget daemon start
+echo "[info] Starting Flexget daemon..."
+
+# Remove lockfile if exists
+if [ -f /config/.flexget-lock ]; then
+        echo "Lockfile found...removing"
+        /bin/rm -f /config/.flexget-lock
+fi
+
+# run deluge daemon
+/usr/bin/flexget daemon start
