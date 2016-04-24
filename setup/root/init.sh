02db5c2426f977a78e28e4a196f85120d842985f
paulpoco <paulpoco@gmail.com>
2016-04-24 14:35:24 -0700

    Update init.sh
    Duplicate the deluge perm.txt sectio for flexget

--- setup/root/init.sh
+++ setup/root/init.sh
@@ -37,6 +37,22 @@ else
 
 fi
 
+# check for presence of perms file, if it exists then skip
+# setting permissions, otherwise recursively set on /home/nobody/.flexget
+if [[ ! -f "/home/nobody/.flexget/perms.txt" ]]; then
+
+	# set permissions for /home/nobody/.flexget volume mapping
+	echo "[info] Setting permissions recursively on /home/nobody/.flexget..."
+	chown -R "${PUID}":"${PGID}" /home/nobody/.flexget
+	chmod -R 775 /home/nobody/.flexget
+	echo "This file prevents permissions from being applied/re-applied to /home/nobody/.flexget, if you want to reset permissions then please delete this file and restart the container." > /config/perms.txt
+
+else
+
+	echo "[info] Permissions already set for /home/nobody/.flexget"
+
+fi
+
 # set permissions inside container
 chown -R "${PUID}":"${PGID}" /usr/bin/deluged /usr/bin/deluge-web /usr/bin/privoxy /etc/privoxy /home/nobody /usr/bin/flexget /usr/sbin/flexget
 chmod -R 775 /usr/bin/deluged /usr/bin/deluge-web /usr/bin/privoxy /etc/privoxy /home/nobody /usr/bin/flexget /usr/sbin/flexget
