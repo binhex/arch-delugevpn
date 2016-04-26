7d3d0c0a8bcd6050df9039782b46aa5bc1fa7a25
paulpoco <paulpoco@gmail.com>
2016-04-26 11:16:40 -0700

    Update flexget.sh
    look for existing config.yml if yes change nothing else add basic config.yml

--- apps/nobody/flexget.sh
+++ apps/nobody/flexget.sh
@@ -8,6 +8,16 @@ if [ -f /home/nobody/.flexget/.config-lock ]; then
         /bin/rm -f /home/nobody/.flexget/.config-lock
 fi
 
+# Check if config.yml exists. If not, copy in
+if [ -f /home/nobody/.flexget/config.yml ]; then
+  echo "Using existing config file."
+else
+  echo "Creating config.yml from template."
+  cp /config.yml  /home/nobody/.flexget/config.yml
+  chown nobody:users /home/nobody/.flexget/config.yml
+  chmod +x /home/nobody/.flexget/config.yml
+fi
+
 # run flexget set webui password and run daemon
 /usr/bin/flexget web passwd flexpass
 /usr/bin/flexget -c /home/nobody/.flexget/config.yml daemon start
