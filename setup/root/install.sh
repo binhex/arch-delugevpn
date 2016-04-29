cd73045790318239935198eac1113e3753460911
paulpoco <paulpoco@gmail.com>
2016-04-28 22:17:02 -0700

    Update install.sh
    add gcc
    then remove gcc a the end

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat python2-pip nano pkg-config"
+pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat python2-pip nano gcc pkg-config"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
@@ -22,6 +22,7 @@ pip2 install --upgrade flexget
 source /root/aor.sh
 
 # cleanup
+pacman -Rs gcc
 yes|pacman -Scc
 rm -rf /usr/share/locale/*
 rm -rf /usr/share/man/*
