56dae95ba2772c2665b4a02bf80e24451b81d27c
paulpoco <paulpoco@gmail.com>
2016-04-28 21:27:38 -0700

    Update install.sh
    #pip2 install --upgrade --force-reinstall requests[security]

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -14,7 +14,7 @@ rm -f /usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg
 
 #install flextget
 pip2 install --upgrade pip
-pip2 install --upgrade --force-reinstall requests[security]
+#pip2 install --upgrade --force-reinstall requests[security]
 pip2 install --upgrade setuptools
 pip2 install --upgrade flexget
 
