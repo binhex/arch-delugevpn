3aeb509e29a94684b21d5aac0ffb2f7f9a48fe82
binhex <megalith01@gmail.com>
2016-06-30 13:29:05 +0100

    fix removal of faulty scheduler egg and install patched ver

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -9,11 +9,14 @@ pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
 
+# call aor script (arch official repo)
+source /root/aor.sh
+
 # remove faulty scheduler plugin (bug with 1.3.12 release)
 rm -f /usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg
 
-# call aor script (arch official repo)
-source /root/aor.sh
+# install patched schedulder plugin from github (remove this for 1.3.13+ release)
+curl -L -o "/usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg" "https://github.com/binhex/arch-patches/raw/master/arch-deluge/plugins/scheduler/Scheduler-0.2-py2.7.egg"
 
 # cleanup
 yes|pacman -Scc
