b636966a130ab3b22fdbf881e7f2e56cfa7d4056
binhex <megalith01@gmail.com>
2016-07-21 22:16:53 +0100

    official 1.3.13 is out, scheduler fixed also

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,19 +4,13 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat deluge"
+pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
 
-# call aor script (arch official repo) - commented out for now to force non dev version, remove comment and deluge from above packages list once official release out
-#source /root/aor.sh
-
-# remove faulty scheduler plugin (bug with 1.3.12 release)
-rm -f /usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg
-
-# install patched schedulder plugin from github (remove this for 1.3.13+ release)
-curl -L -o "/usr/lib/python2.7/site-packages/deluge/plugins/Scheduler-0.2-py2.7.egg" "https://github.com/binhex/arch-patches/raw/master/arch-deluge/plugins/scheduler/Scheduler-0.2-py2.7.egg"
+# call aor script (arch official repo)
+source /root/aor.sh
 
 # cleanup
 yes|pacman -Scc
