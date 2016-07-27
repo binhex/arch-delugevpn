e0a284028f6cdd687e0df22b49916a964e0a6a61
binhex <megalith01@gmail.com>
2016-07-27 14:59:33 +0100

    remove .dev0 from compiled package

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -12,6 +12,10 @@ pacman -S --needed $pacman_packages --noconfirm
 # call aor script (arch official repo)
 source /root/aor.sh
 
+# manually remove .dev0 from compiled package name (is a result of pull commit from github)
+mv "/usr/lib/python2.7/site-packages/deluge-1.3.13.dev0-py2.7.egg-info/" "/usr/lib/python2.7/site-packages/deluge-1.3.13-py2.7.egg-info/"
+sed -i -e 's~\.dev0~~g' "/usr/lib/python2.7/site-packages/deluge-1.3.13-py2.7.egg-info/PKG-INFO" "/usr/bin/deluge" "/usr/bin/deluge-console" "/usr/bin/deluged" "/usr/bin/deluge-gtk" "/usr/bin/deluge-web"
+
 # cleanup
 yes|pacman -Scc
 rm -rf /usr/share/locale/*
