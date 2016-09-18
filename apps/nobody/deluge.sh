76d988da37ec679918aa97e06aa91f12ca095491
paulpoco <paulpoco@gmail.com>
2016-09-18 02:59:12 -0700

    Update deluge.sh

--- apps/nobody/deluge.sh
+++ apps/nobody/deluge.sh
@@ -55,156 +55,165 @@ else
 		# run scripts to identity vpn ip
 		source /home/nobody/getvpnip.sh
 
-		# check deluge is running, if not then set to first_run and reload
-		if ! pgrep -f /usr/bin/deluged > /dev/null; then
+		# if vpn_ip is not blank then run, otherwise log warning
+		if [[ ! -z "${vpn_ip}" ]]; then
 
-			echo "[info] Deluge daemon not running, marking as first run"
+			# check deluge is running, if not then set to first_run and reload
+			if ! pgrep -f /usr/bin/deluged > /dev/null; then
 
-			# mark as first run and reload required due to deluge not running
-			first_run="true"
-			reload="true"
+				echo "[info] Deluge daemon not running, marking as first run"
 
-		else
-
-			# if current bind interface ip is different to tunnel local ip then re-configure deluge
-			if [[ $deluge_ip != "$vpn_ip" ]]; then
-
-				echo "[info] Deluge listening interface IP $deluge_ip and VPN provider IP different, marking for reload"
-
-				# mark as reload required due to mismatch
-				first_run="false"
-				reload="true"
-
-			fi
-
-		fi
-
-		if [[ $VPN_PROV == "pia" ]]; then
-
-			# run scripts to identify vpn port
-			source /home/nobody/getvpnport.sh
-
-			if [[ $first_run == "false" ]]; then
-
-				# if vpn port is not an integer then log warning
-				if [[ ! $vpn_port =~ ^-?[0-9]+$ ]]; then
-
-					echo "[warn] PIA incoming port is not an integer, downloads will be slow, does PIA remote gateway supports port forwarding?"
-
-					# set vpn port to current deluge port, as we currently cannot detect incoming port (line saturated, or issues with pia)
-					vpn_port="${deluge_port}"
-
-				elif [[ $deluge_port != "$vpn_port" ]]; then
-
-					echo "[info] Deluge incoming port $deluge_port and VPN incoming port $vpn_port different, marking for reload"
-
-					# mark as reload required due to mismatch
-					first_run="false"
-					reload="true"
-
-				# run netcat to identify if port still open, use exit code
-				nc_exitcode=$(/usr/bin/nc -z -w 3 "${deluge_ip}" "${deluge_port}")
-
-				elif [[ "${nc_exitcode}" -ne 0 ]]; then
-
-					echo "[info] Deluge incoming port closed, marking for reload"
-
-					# mark as reload required due to mismatch
-					first_run="false"
-					reload="true"
-
-				fi
-
-			else
-
-				# if vpn port is not an integer then log warning
-				if [[ ! $vpn_port =~ ^-?[0-9]+$ ]]; then
-
-					echo "[warn] PIA incoming port is not an integer, downloads will be slow, does PIA remote gateway supports port forwarding?"
-
-				fi
-
-				# mark as reload required due to first run
+				# mark as first run and reload required due to deluge not running
 				first_run="true"
 				reload="true"
 
-			fi
-
-		fi
-
-		if [[ $reload == "true" ]]; then
-
-			if [[ $first_run == "false" ]]; then
-
-				echo "[info] Reload required, configuring Deluge..."
-
-				# set listen interface to tunnel local ip using command line
-				/usr/bin/deluge-console -c /config "config --set listen_interface $vpn_ip"
-
-				# set deluge ip to current vpn ip (used when checking for changes on next run)
-				deluge_ip="${vpn_ip}"
-
-				if [[ $VPN_PROV == "pia" ]]; then
-
-					# enable bind incoming port to specific port (disable random)
-					/usr/bin/deluge-console -c /config "config --set random_port False"
-
-					# set incoming port
-					/usr/bin/deluge-console -c /config "config --set listen_ports ($vpn_port,$vpn_port)"
-					
-					# set deluge port to current vpn port (used when checking for changes on next run)
-					deluge_port="${vpn_port}"
-
-
-				fi
-
 			else
 
-				# set listen interface ip address for deluge using sed
-				sed -i -e 's~"listen_interface":\s*"[^"]*~"listen_interface": "'"${vpn_ip}"'~g' /config/core.conf
+				# if current bind interface ip is different to tunnel local ip then re-configure deluge
+				if [[ $deluge_ip != "$vpn_ip" ]]; then
 
-				# set deluge ip to current vpn ip (used when checking for changes on next run)
-				deluge_ip="${vpn_ip}"
+					echo "[info] Deluge listening interface IP $deluge_ip and VPN provider IP different, marking for reload"
 
-				echo "[info] All checks complete, starting Deluge..."
-				
-				# run deluge daemon (daemonized, non-blocking)
-				/usr/bin/deluged -c /config -L info -l /config/deluged.log
-
-				if [[ $VPN_PROV == "pia" ]]; then
-
-					# wait for deluge daemon process to start (listen for port)
-					while [[ $(netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".58846"') == "" ]]; do
-						sleep 0.1
-					done
-
-					# enable bind incoming port to specific port (disable random)
-					/usr/bin/deluge-console -c /config "config --set random_port False"
-
-					# set incoming port
-					/usr/bin/deluge-console -c /config "config --set listen_ports ($vpn_port,$vpn_port)"
-
-					# set deluge port to current vpn port (used when checking for changes on next run)
-					deluge_port="${vpn_port}"
+					# mark as reload required due to mismatch
+					first_run="false"
+					reload="true"
 
 				fi
 
 			fi
 
+			if [[ $VPN_PROV == "pia" ]]; then
+
+				# run scripts to identify vpn port
+				source /home/nobody/getvpnport.sh
+
+				if [[ $first_run == "false" ]]; then
+
+					# if vpn port is not an integer then log warning
+					if [[ ! $vpn_port =~ ^-?[0-9]+$ ]]; then
+
+						echo "[warn] PIA incoming port is not an integer, downloads will be slow, does PIA remote gateway supports port forwarding?"
+
+						# set vpn port to current deluge port, as we currently cannot detect incoming port (line saturated, or issues with pia)
+						vpn_port="${deluge_port}"
+
+					elif [[ $deluge_port != "$vpn_port" ]]; then
+
+						echo "[info] Deluge incoming port $deluge_port and VPN incoming port $vpn_port different, marking for reload"
+
+						# mark as reload required due to mismatch
+						first_run="false"
+						reload="true"
+
+					# run netcat to identify if port still open, use exit code
+					nc_exitcode=$(/usr/bin/nc -z -w 3 "${deluge_ip}" "${deluge_port}")
+
+					elif [[ "${nc_exitcode}" -ne 0 ]]; then
+
+						echo "[info] Deluge incoming port closed, marking for reload"
+
+						# mark as reload required due to mismatch
+						first_run="false"
+						reload="true"
+
+					fi
+
+				else
+
+					# if vpn port is not an integer then log warning
+					if [[ ! $vpn_port =~ ^-?[0-9]+$ ]]; then
+
+						echo "[warn] PIA incoming port is not an integer, downloads will be slow, does PIA remote gateway supports port forwarding?"
+
+					fi
+
+					# mark as reload required due to first run
+					first_run="true"
+					reload="true"
+
+				fi
+
+			fi
+
+			if [[ $reload == "true" ]]; then
+
+				if [[ $first_run == "false" ]]; then
+
+					echo "[info] Reload required, configuring Deluge..."
+
+					# set listen interface to tunnel local ip using command line
+					/usr/bin/deluge-console -c /config "config --set listen_interface $vpn_ip"
+
+					# set deluge ip to current vpn ip (used when checking for changes on next run)
+					deluge_ip="${vpn_ip}"
+
+					if [[ $VPN_PROV == "pia" ]]; then
+
+						# enable bind incoming port to specific port (disable random)
+						/usr/bin/deluge-console -c /config "config --set random_port False"
+
+						# set incoming port
+						/usr/bin/deluge-console -c /config "config --set listen_ports ($vpn_port,$vpn_port)"
+						
+						# set deluge port to current vpn port (used when checking for changes on next run)
+						deluge_port="${vpn_port}"
+
+
+					fi
+
+				else
+
+					# set listen interface ip address for deluge using sed
+					sed -i -e 's~"listen_interface":\s*"[^"]*~"listen_interface": "'"${vpn_ip}"'~g' /config/core.conf
+
+					# set deluge ip to current vpn ip (used when checking for changes on next run)
+					deluge_ip="${vpn_ip}"
+
+					echo "[info] All checks complete, starting Deluge..."
+					
+					# run deluge daemon (daemonized, non-blocking)
+					/usr/bin/deluged -c /config -L info -l /config/deluged.log
+
+					if [[ $VPN_PROV == "pia" ]]; then
+
+						# wait for deluge daemon process to start (listen for port)
+						while [[ $(netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".58846"') == "" ]]; do
+							sleep 0.1
+						done
+
+						# enable bind incoming port to specific port (disable random)
+						/usr/bin/deluge-console -c /config "config --set random_port False"
+
+						# set incoming port
+						/usr/bin/deluge-console -c /config "config --set listen_ports ($vpn_port,$vpn_port)"
+
+						# set deluge port to current vpn port (used when checking for changes on next run)
+						deluge_port="${vpn_port}"
+
+					fi
+
+				fi
+
+			fi
+
+			if [[ "${DEBUG}" == "true" ]]; then
+
+				echo "[debug] VPN incoming port is $vpn_port"
+				echo "[debug] Deluge incoming port is $deluge_port"
+				echo "[debug] VPN IP is $vpn_ip"
+				echo "[debug] Deluge IP is $deluge_ip"
+
+			fi
+
+			# reset triggers to negative values
+			first_run="false"
+			reload="false"
+
+		else
+
+			echo "[warn] VPN IP not detected"
+
 		fi
-
-		if [[ "${DEBUG}" == "true" ]]; then
-
-			echo "[debug] VPN incoming port is $vpn_port"
-			echo "[debug] Deluge incoming port is $deluge_port"
-			echo "[debug] VPN IP is $vpn_ip"
-			echo "[debug] Deluge IP is $deluge_ip"
-
-		fi
-
-		# reset triggers to negative values
-		first_run="false"
-		reload="false"
 
 		echo "[info] Sleeping for ${sleep_period} mins before rechecking listen interface and port (port checking is for PIA only)"
 		sleep "${sleep_period}"m
