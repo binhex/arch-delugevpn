3899e30d93afd10bb40125467d8f2a555cbfc5c0
paulpoco <paulpoco@gmail.com>
2016-09-18 04:55:14 -0700

    Update install.sh

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat python2-pip nano gcc pkg-config"
+pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat python2-pip nano gcc pkg-config deluge freetype2"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
@@ -16,11 +16,11 @@ pip2 install --upgrade setuptools
 pip2 install --upgrade flexget
 
 # call aor script (arch official repo) - commented out for now to force non dev version, remove comment and deluge from above packages list once official release out
-source /root/aor.sh
+# source /root/aor.sh
 
 # manually remove .dev0 from compiled package name (is a result of pull commit from github)
-mv "/usr/lib/python2.7/site-packages/deluge-1.3.13.dev0-py2.7.egg-info/" "/usr/lib/python2.7/site-packages/deluge-1.3.13-py2.7.egg-info/"
-sed -i -e 's~\.dev0~~g' "/usr/lib/python2.7/site-packages/deluge-1.3.13-py2.7.egg-info/PKG-INFO" "/usr/bin/deluge" "/usr/bin/deluge-console" "/usr/bin/deluged" "/usr/bin/deluge-gtk" "/usr/bin/deluge-web"
+#mv "/usr/lib/python2.7/site-packages/deluge-1.3.13.dev0-py2.7.egg-info/" "/usr/lib/python2.7/site-packages/deluge-1.3.13-py2.7.egg-info/"
+#sed -i -e 's~\.dev0~~g' "/usr/lib/python2.7/site-packages/deluge-1.3.13-py2.7.egg-info/PKG-INFO" "/usr/bin/deluge" "/usr/bin/deluge-console" "/usr/bin/deluged" "/usr/bin/deluge-gtk" "/usr/bin/deluge-web"
 
 # cleanup
 yes|pacman -Rs gcc
