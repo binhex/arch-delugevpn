ca9a71fba51f71947f74ea4661f7b0c52ff7a87e
paulpoco <paulpoco@gmail.com>
2016-09-18 03:02:31 -0700

    Update checkip.sh

--- apps/nobody/checkip.sh
+++ apps/nobody/checkip.sh
@@ -3,15 +3,15 @@
 # create function to check tunnel local ip is valid
 check_valid_ip() {
 
-    IP_ADDRESS="$1"
-	
-    # check if the format looks right
-    echo "$IP_ADDRESS" | egrep -qE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' || return 1
-	
-    # check that each octect is less than or equal to 255
-    echo $IP_ADDRESS | awk -F'.' '$1 <=255 && $2 <= 255 && $3 <=255 && $4 <= 255 {print "Y" } ' | grep -q Y || return 1
-	
-    return 0
+	IP_ADDRESS="$1"
+
+	# check if the format looks right
+	echo "$IP_ADDRESS" | egrep -qE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' || return 1
+
+	# check that each octect is less than or equal to 255
+	echo $IP_ADDRESS | awk -F'.' '$1 <=255 && $2 <= 255 && $3 <=255 && $4 <= 255 {print "Y" } ' | grep -q Y || return 1
+
+	return 0
 }
 
 # loop and wait until adapter tun0 local ip is valid
