00f95f62e1b4a421c5c5cefdadb7e1076b7dae13
paulpoco <paulpoco@gmail.com>
2016-09-18 03:09:54 -0700

    Update install.sh

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,7 +4,7 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat python2-pip nano gcc pkg-config"
+pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2-notify gnu-netcat python2-pip nano gcc pkg-config deluge"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
@@ -18,8 +18,8 @@ pip2 install --upgrade --force-reinstall requests[security]
 pip2 install --upgrade setuptools
 pip2 install --upgrade flexget
 
-# call aor script (arch official repo)
-source /root/aor.sh
+# call aor script (arch official repo) - commented out for now to force non dev version, remove comment and deluge from above packages list once official release out
+#source /root/aor.sh
 
 # cleanup
 yes|pacman -Rs gcc
