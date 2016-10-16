c31b962f23e2a815f608ed23774a1d262d18679d
binhex <megalith01@gmail.com>
2016-10-16 21:48:04 +0100

    install from uploaded github stable version

--- setup/root/init.sh
+++ setup/root/init.sh
@@ -44,4 +44,4 @@ chmod -R 775 /usr/bin/deluged /usr/bin/deluge-web /usr/bin/privoxy /etc/privoxy 
 echo "[info] Starting Supervisor..."
 
 # run supervisor
-"/usr/bin/supervisord" -c "/etc/supervisor.conf" -n
+exec /usr/bin/supervisord -c /etc/supervisor.conf -n
--- setup/root/install.sh
+++ setup/root/install.sh
@@ -10,7 +10,11 @@ pacman_packages="unzip unrar pygtk python2-service-identity python2-mako python2
 pacman -S --needed $pacman_packages --noconfirm
 
 # call aor script (arch official repo)
-source /root/aor.sh
+#source /root/aor.sh
+
+# manually download stable package from binhex repo
+curl -o /tmp/deluge-1.3.11-3-any.pkg.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/deluge-1.3.11-3-any.pkg.tar.xz
+pacman -U /tmp/deluge-1.3.11-3-any.pkg.tar.xz --noconfirm
 
 # manually remove .dev0 from compiled package name (is a result of pull commit from github)
 mv "/usr/lib/python2.7/site-packages/deluge-1.3.13.dev0-py2.7.egg-info/" "/usr/lib/python2.7/site-packages/deluge-1.3.13-py2.7.egg-info/"
