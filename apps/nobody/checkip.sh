fd714de0c79edfc13c62c401aaee6f1d8298ba3d
binhex <megalith01@gmail.com>
2016-10-17 17:50:00 +0100

    add tunnel device to readme, tweaks to vars

--- apps/nobody/checkip.sh
+++ apps/nobody/checkip.sh
@@ -19,5 +19,5 @@ vpn_ip=""
 while ! check_valid_ip "$vpn_ip"
 do
 	sleep 0.1
-	vpn_ip=`ifconfig "${VPN_DEVICE_TYPE}"0 2>/dev/null | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+'`
+	vpn_ip=$(ifconfig "${VPN_DEVICE_TYPE}0" 2>/dev/null | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+')
 done
