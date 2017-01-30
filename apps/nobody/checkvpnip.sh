50ef370b48682ba27e0c67948102249cc2e3618a
binhex <megalith01@gmail.com>
2017-01-30 17:00:43 +0000

    additional loggins for tunnel ip

--- apps/nobody/checkvpnip.sh
+++ apps/nobody/checkvpnip.sh
@@ -14,6 +14,8 @@ check_valid_ip() {
 	return 0
 }
 
+echo "[info] Waiting for valid IP address from tunnel..."
+
 # loop and wait until tunnel adapter local ip is valid
 vpn_ip=""
 while ! check_valid_ip "$vpn_ip"
@@ -21,3 +23,5 @@ do
 	sleep 0.1
 	vpn_ip=$(ifconfig "${VPN_DEVICE_TYPE}0" 2>/dev/null | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+')
 done
+
+echo "[info] IP address from tunnel valid"
