24ab26dda4170437916c579d6b63e3c8c74c20e1
paulpoco <paulpoco@gmail.com>
2017-02-11 11:11:12 -0800

    Update checkip.sh
    switch to env var to define tunnel device

--- apps/nobody/checkip.sh
+++ apps/nobody/checkip.sh
@@ -14,10 +14,10 @@ check_valid_ip() {
 	return 0
 }
 
-# loop and wait until adapter tun0 local ip is valid
+# loop and wait until tunnel adapter local ip is valid
 vpn_ip=""
 while ! check_valid_ip "$vpn_ip"
 do
 	sleep 0.1
-	vpn_ip=`ifconfig tun0 2>/dev/null | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+'`
+	vpn_ip=`ifconfig "${VPN_DEVICE_TYPE}"0 2>/dev/null | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+'`
 done
