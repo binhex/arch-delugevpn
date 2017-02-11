be85dfd6ad7d35dbff30db475175503e6da0b660
paulpoco <paulpoco@gmail.com>
2017-02-11 11:08:11 -0800

    Update install.sh
    correct url

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -19,7 +19,7 @@ pip2 install --upgrade flexget
 #source /root/aor.sh
 
 # manually download stable package from binhex repo
-curl -o /tmp/deluge-1.3.11-3-any.pkg.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/deluge-1.3.11-3-any.pkg.tar.xz
+curl -o /tmp/deluge-1.3.11-3-any.pkg.tar.xz -L https://github.com/binhex/arch-packages/raw/master/compiled/deluge-1.3.13-1-any.pkg.tar.xz
 pacman -U /tmp/deluge-1.3.11-3-any.pkg.tar.xz --noconfirm
 
 # manually remove .dev0 from compiled package name (is a result of pull commit from github)
