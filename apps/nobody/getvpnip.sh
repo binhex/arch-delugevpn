3602345ad70d4217d32c56065a31aed6d720aebe
paulpoco <paulpoco@gmail.com>
2017-02-11 11:13:56 -0800

    Update getvpnip.sh
    switch to env var to define tunnel device

--- apps/nobody/getvpnip.sh
+++ apps/nobody/getvpnip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# get currently allocated ip address for adapter tun0
-vpn_ip=`ifconfig tun0 2>/dev/null | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+'`
+# get currently allocated ip address for tunnel adapter
+vpn_ip=`ifconfig "${VPN_DEVICE_TYPE}"0 2>/dev/null | grep 'inet' | grep -P -o -m 1 '(?<=inet\s)[^\s]+'`
