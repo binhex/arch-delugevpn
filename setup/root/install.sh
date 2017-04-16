fafdf900aa271bdaadd5ed0c20b316c7a7ff123c
binhex <megalith01@gmail.com>
2017-04-16 20:39:57 +0100

    put in curl retry and backoff

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -7,7 +7,7 @@ set -e
 ####
 
 # download build scripts from github
-curl -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
 
 # unzip build scripts
 unzip /tmp/scripts-master.zip -d /tmp
