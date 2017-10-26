5eb58d885802a5ff4ec5d41bb0e8bbd55aba4bbe
Erik Lindskog <erik.lindskog@codemill.se>
2017-10-26 20:30:06 +0200

    Add flexget

--- build/root/install.sh
+++ build/root/install.sh
@@ -25,7 +25,7 @@ source /root/custom.sh
 ####
 
 # define pacman packages
-pacman_packages="pygtk python2-service-identity python2-mako python2-notify gnu-netcat ipcalc"
+pacman_packages="pygtk python2-service-identity python2-mako python2-notify gnu-netcat ipcalc python2-pip"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
@@ -49,6 +49,15 @@ aur_packages=""
 
 # call aur install script (arch user repo)
 source /root/aur.sh
+
+# pip packages
+####
+pip_packages="flexget"
+
+# install pip packages
+if [[ ! -z "${pip_packages}" ]]; then
+    yes | pip2 install ${pip_packages}
+fi
 
 # container perms
 ####
