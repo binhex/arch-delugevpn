3bcbc0ee80f2ba8bd1a74bf4763be0aa96cb3f63
binhex <megalith01@gmail.com>
2017-11-02 15:09:20 +0000

    remove build scripts, now in openvpn

--- build/root/install.sh
+++ build/root/install.sh
@@ -3,18 +3,6 @@
 # exit script if return code != 0
 set -e
 
-# build scripts
-####
-
-# download build scripts from github
-curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
-
-# unzip build scripts
-unzip /tmp/scripts-master.zip -d /tmp
-
-# move shell scripts to /root
-mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
-
 # custom scripts
 ####
 
