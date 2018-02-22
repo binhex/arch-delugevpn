24ead692506c4391a6f8bc4f80f87880491590bf
binhex <pauleccleston@uk-mac-paule.i2inc.org>
2018-02-22 10:36:27 +0000

    remove mac os resource forks

--- build/root/install.sh
+++ build/root/install.sh
@@ -101,6 +101,9 @@ if [[ $VPN_ENABLED == "yes" ]]; then
 		echo "[warn] Unable to chown/chmod /config/openvpn/, assuming SMB mountpoint" | ts '%Y-%m-%d %H:%M:%.S'
 	fi
 
+	# force removal of mac os resource fork files in ovpn folder
+	rm -rf /config/openvpn/._*.ovpn
+	
 	# wildcard search for openvpn config files (match on first result)
 	export VPN_CONFIG=$(find /config/openvpn -maxdepth 1 -name "*.ovpn" -print -quit)
 
