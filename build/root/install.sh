2dbd933b48218e74c27843188dd5020cdefb8d76
binhex <megalith01@gmail.com>
2019-06-10 21:43:58 +0100

    revert back to direct call to dos2unix

--- build/root/install.sh
+++ build/root/install.sh
@@ -180,7 +180,7 @@ if [[ $VPN_ENABLED == "yes" ]]; then
 	echo "[info] OpenVPN config file (ovpn extension) is located at ${VPN_CONFIG}" | ts '%Y-%m-%d %H:%M:%.S'
 
 	# convert CRLF (windows) to LF (unix) for ovpn
-	/root/dos2unix-wrapper.sh "${VPN_CONFIG}"
+	/usr/bin/dos2unix "${VPN_CONFIG}"
 
 	# get first matching 'remote' line in ovpn
 	vpn_remote_line=$(cat "${VPN_CONFIG}" | grep -P -o -m 1 '^(\s+)?remote\s.*')
