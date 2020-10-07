af2779630c710597e8182512d2ff8d529d3c536a
binhex <megalith01@gmail.com>
2020-10-07 21:32:59 +0100

    remove build scripts to prevent overwrite of init.sh

--- build/root/install.sh
+++ build/root/install.sh
@@ -3,17 +3,7 @@
 # exit script if return code != 0
 set -e
 
-# build scripts
-####
-
-# download build scripts from github
-curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
-
-# unzip build scripts
-unzip /tmp/scripts-master.zip -d /tmp
-
-# move shell scripts to /root
-mv /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
+# note do NOT download build scripts - inherited from int script with envvars common defined
 
 # detect image arch
 ####
