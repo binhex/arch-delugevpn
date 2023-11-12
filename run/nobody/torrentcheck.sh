d7f3ccad70ba2afec370e9e6b6620da6de820e80
binhex <megalith01@gmail.com>
2023-11-12 21:10:03 +0000

    fix torrent check state for later ver of deluge

--- run/nobody/torrentcheck.sh
+++ run/nobody/torrentcheck.sh
@@ -4,7 +4,7 @@
 # start Deluge. This script must be run as the user running the Deluge daemon (deluged).
 
 # identify any torrents with state error and save these torrent id's as an array
-torrent_id_error_array=( $(deluge-console -c /config "info" | grep -B 1 'State: Error' | xargs | grep -P -o -m 1 '(?<=ID:\s)[^\s]+' | xargs) )
+torrent_id_error_array=( $(deluge-console -c /config "info -v" | grep -B 1 'State: Error' | xargs | grep -P -o -m 1 '(?<=ID:\s)[^\s]+' | xargs) )
 
 if [[ -n ${torrent_id_error_array[@]} ]]; then
 
