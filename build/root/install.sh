762add8e2a9659c1a9b19951395d11d2d7764adb
Paul V. <49482721+prolife86@users.noreply.github.com>
2025-05-06 08:29:52 +0200

    Update install.sh
    updated to 2.2.0 useragent tag (required for certain prvate trackers).

--- build/root/install.sh
+++ build/root/install.sh
@@ -74,7 +74,7 @@ mkdir -p /home/nobody/.cache/Python-Eggs
 chmod -R 700 /home/nobody/.cache/Python-Eggs
 
 # change peerid to appear to be 2.1.1 stable - note this does not work for all/any private trackers at present
-sed -i -e "s~peer_id = substitute_chr(peer_id, 6, release_chr)~peer_id = \'-DE211s-\'\n        release_chr = \'s\'~g" /usr/lib/python3*/site-packages/deluge/core/core.py
+sed -i -e "s~peer_id = substitute_chr(peer_id, 6, release_chr)~peer_id = \'-DE220s-\'\n        release_chr = \'s\'~g" /usr/lib/python3*/site-packages/deluge/core/core.py
 
 # container perms
 ####
