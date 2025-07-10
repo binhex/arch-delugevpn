2b907b28f1faf65f8e1746281b0b8dd7188f9162
binhex <megalith01@gmail.com>
2025-07-10 10:14:47 +0100

    various path fixes for scripts

--- build/root/install.sh
+++ build/root/install.sh
@@ -129,7 +129,7 @@ EOF
 sed -i '/# PERMISSIONS_PLACEHOLDER/{
     s/# PERMISSIONS_PLACEHOLDER//g
     r /tmp/permissions_heredoc
-}' /usr/local/bin/init.sh
+}' /usr/local/bin/shell/docker/init.sh
 rm /tmp/permissions_heredoc
 
 # env vars
@@ -169,7 +169,7 @@ EOF
 sed -i '/# ENVVARS_PLACEHOLDER/{
     s/# ENVVARS_PLACEHOLDER//g
     r /tmp/envvars_heredoc
-}' /usr/local/bin/init.sh
+}' /usr/local/bin/shell/docker/init.sh
 rm /tmp/envvars_heredoc
 
 # cleanup
